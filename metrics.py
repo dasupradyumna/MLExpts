c60ae0261e38e84f2065d18782d3470581914782
pradyudasu <pradyu1104@gmail.com>
2021-07-14 21:54:15 +0530

    Implemented MultiSVM gradient calculation in metrics.py

--- metrics.py
+++ metrics.py
@@ -17,18 +17,20 @@ def ManhattanNorm( x, y ) :
     return np.sum(abs(x - y), axis=k)
 
 
-def MultiSVMLoss( scores, labels ) :
-    # scores = N x C float array, labels = N x C bool array
-    # labels = labels.astype(bool)
+def MultiSVMLossWithGrad( datapoints, labels, scores ) :
+    # scores : N x C float array, labels : N x C bool array
     true_label_scores = scores[labels].reshape((-1, 1))  # N x 1, contains scores corresponding to true labels
     loss = np.maximum(0, scores - true_label_scores + 1)  # N x C, loss for each datapoint and each class label
     loss[labels] = 0  # setting loss value for true labels to zero
+    loss_01 = (loss > 0).astype(int)  # N x C, loss > 0 : 1 else 0 (derivative of Hinge loss)
     loss = np.sum(loss, axis=1)  # N-vector, summing up loss over all classes for each datapoint
     loss = np.mean(loss)  # scalar, taking average of losses over all datapoints
 
-    gradient = 0
+    # N-vector, setting loss_01 values for true labels equal to sum of ones in each row (datapoint)
+    loss_01[labels] = -np.sum(loss_01, axis=1)
+    gradients = datapoints.T @ loss_01
 
-    return loss, gradient
+    return loss, gradients
 
 
 def SoftmaxLoss( ) :

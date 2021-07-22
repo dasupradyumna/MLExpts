33785a5480259ae9d6c588c3107121e6682ae91f
pradyudasu <pradyu1104@gmail.com>
2021-07-22 17:32:40 +0530

    Added Softmax and ReLU classes (for NN activation functions) Modified MultiSVMLoss and CrossEntropyLoss (now, SparseCELoss) - Use forward and backward naming convention for their methods Updated gradient_check() function to work with NNs

--- metrics.py
+++ metrics.py
@@ -7,25 +7,30 @@ import numpy as np
 
 # square-root of sum of squares of differences
 def EuclideanNorm( x, y ) :
+    assert x.ndim <= 2 and y.ndim <= 2, \
+        "Expected argument is at most a 2D numpy array."
     axis = int(x.ndim == 2 or y.ndim == 2)
     return np.sqrt(np.sum((x - y) * (x - y), axis))
 
 
 # sum of magnitudes of differences
 def ManhattanNorm( x, y ) :
+    assert x.ndim <= 2 and y.ndim <= 2, \
+        "Expected argument is at most a 2D numpy array."
     axis = int(x.ndim == 2 or y.ndim == 2)
     return np.sum(abs(x - y), axis)
 
 
 # calculates gradients numerically using first principle; sanity check for analytical gradient functions
-def gradient_check( weights, datapoints, labels, loss_model ) :
-    current_loss = loss_model.evaluate(weights, datapoints, labels)
+# datapoints : input to model, weights : target of gradient, labels : output of model
+def gradient_check( model, datapoints, weights, labels ) :
+    _, current_loss = model.forward(datapoints, labels)
     gradients = np.zeros(weights.shape)
     h = 1e-8  # perturbation value
-    for row in range(weights.shape[0]) :
-        for col in range(weights.shape[1]) :
+    for row in range(gradients.shape[0]) :
+        for col in range(gradients.shape[1]) :
             weights[row, col] += h  # disturb each weight value by h
-            new_loss = loss_model.evaluate(weights, datapoints, labels)  # perturbed loss value
+            _, new_loss = model.forward(datapoints, labels)  # perturbed loss value
             gradients[row, col] = (new_loss - current_loss) / h  # first principles of differentiation
             weights[row, col] -= h  # restoring weight matrix
 
@@ -33,8 +38,6 @@ def gradient_check( weights, datapoints, labels, loss_model ) :
 
 
 # Multi-class SVM (Hinge) Loss
-# datapoints : N x K float array, labels : N x C bool array, scores : N x C float array
-# methods for calculating loss and gradient
 class MultiSVMLoss :
 
     def __init__( self, RegularizationLambda, MarginThreshold ) :
@@ -42,56 +45,86 @@ class MultiSVMLoss :
         self.margin = MarginThreshold  # threshold for zero loss
         self.loss_cache = None  # used in gradient calculation
 
-    def evaluate( self, weights, datapoints, labels ) :
-        scores = datapoints @ weights  # X.W matrix multiplication
-        true_label_scores = scores[labels].reshape((-1, 1))  # N x 1, contains scores corresponding to true labels
+    # calculate loss based from the output scores from model
+    # scores : N x C array, labels : N-vector, reg_loss : loss summed up from regularization of weights
+    def forward( self, scores, labels, reg_loss ) :
+        true_label_scores = scores[np.arange(labels.size), labels].reshape(
+            (-1, 1)
+        )  # N x 1, contains scores corresponding to true labels
         loss = np.maximum(0, scores - true_label_scores + self.margin)  # N x C, loss for each datapoint and each class
-        loss[labels] = 0  # setting loss value for true labels to zero
+        loss[np.arange(labels.size), labels] = 0  # setting loss value for true labels to zero
         self.loss_cache = (loss > 0)  # caching binary loss values, loss > 0 : 1 else 0 (derivative of Hinge loss)
         loss = np.sum(loss, axis=1)  # N-vector, summing up loss over all classes for each datapoint
         loss = np.mean(loss)  # scalar, taking average of losses over all datapoints
-        loss += 0.5 * self.reg_lambda * np.sum(weights * weights)  # regularization term
+        loss += self.reg_lambda * reg_loss  # regularization term
 
         return loss
 
-    def gradient( self, weights, datapoints, labels ) :
-        if self.loss_cache is None : return np.zeros(weights.shape)
-
-        loss_01 = self.loss_cache.astype(int)  # N x C, extracting cached loss
+    # calculate gradients of loss wrt the output scores of model
+    # labels : N-vector
+    def backward( self, _, labels ) :
+        gradients = self.loss_cache.astype(int)  # N x C, extracting cached loss
         # N-vector, setting loss_01 values for true labels equal to sum of ones in each row (datapoint)
-        loss_01[labels] = -np.sum(loss_01, axis=1)
-        gradients = (datapoints.T @ loss_01) / datapoints.shape[0]  # multiplying by X, dividing by no. of datapoints
-        gradients += self.reg_lambda * weights  # derivative of regularization term
+        gradients[np.arange(labels.size), labels] = -np.sum(gradients, axis=1)
 
-        return gradients
+        return gradients, self.reg_lambda
 
 
-# Cross-entropy Loss, using softmax
-# datapoints : N x K float array, labels : N x C bool array, scores : N x C float array
-# methods for calculating loss and gradient
-class CrossEntropyLoss :
+# Cross-Entropy Loss, implemented without one-hot vectors (sparse)
+class SparseCELoss :
 
     def __init__( self, RegularizationLambda ) :
         self.reg_lambda = RegularizationLambda  # regularization hyperparameter
-        self.softmax_cache = None  # used in gradient calculation
 
-    def evaluate( self, weights, datapoints, labels ) :
-        softmax_scores = np.exp(datapoints @ weights)  # N x C, e^s for each element s from X.W matrix multiplication
-        softmax_scores = softmax_scores / np.sum(softmax_scores, axis=1, keepdims=True)  # N x C, softmax distributions
-        self.softmax_cache = softmax_scores  # caching softmax scores
-        loss = -np.log(softmax_scores[labels])  # N-vector, logs of scores of true labels
+    # calculate loss based from the output scores from model
+    # softmax_scores : N x C array, labels : N-vector, reg_loss : loss summed up from regularization of weights
+    def forward( self, softmax_scores, labels, reg_loss ) :
+        loss = - np.log(softmax_scores[np.arange(labels.size), labels])
         loss = np.mean(loss)  # scalar, taking average of losses over all datapoints
-        loss += 0.5 * self.reg_lambda * np.sum(weights * weights)  # regularization term
+        loss += self.reg_lambda * reg_loss  # regularization term
 
         return loss
 
-    def gradient( self, weights, datapoints, labels ) :
-        if self.softmax_cache is None : return np.zeros(weights.shape)
+    # calculate gradients of loss wrt the output scores of model
+    # softmax_scores : N x C array, labels : N-vector
+    def backward( self, softmax_scores, labels ) :
+        gradients = np.zeros(softmax_scores.shape)
+        gradients[np.arange(labels.size), labels] = - 1 / softmax_scores[np.arange(labels.size), labels]
+        return gradients, self.reg_lambda
 
-        softmax_scores = self.softmax_cache  # extracting cached scores
-        softmax_scores[labels] -= 1  # subtracting only scores corresponding to true class for each datapoint by 1
-        gradients = (datapoints.T @ softmax_scores) / datapoints.shape[0]  # derivative of CE Loss
-        gradients += self.reg_lambda * weights  # derivative of regularization term
-        self.softmax_cache = None
 
+# Softmax activation
+class Softmax :
+
+    # calculates softmax distribution from scores
+    # scores : N x C array
+    @staticmethod
+    def forward( scores ) :
+        axis = int(scores.ndim == 2)
+        softmax_scores = np.exp(scores)  # N x C, e^s for each element s from X.W matrix multiplication
+        return softmax_scores / np.sum(softmax_scores, axis, keepdims=True)  # N x C, softmax distributions
+
+    # calculates gradients of distribution wrt scores
+    @staticmethod
+    def backward( gradients, scores ) :
+        num_nodes = scores.shape[1]
+        softmax_grads = - scores.reshape((-1, num_nodes, 1)) @ scores.reshape((-1, 1, num_nodes))
+        idx = np.arange(num_nodes)
+        softmax_grads[:, idx, idx] += scores[:, idx]
+        gradients = gradients.reshape((-1, 1, num_nodes)) @ softmax_grads
+        return gradients.reshape((-1, num_nodes))
+
+
+# ReLU activation
+class ReLU :
+
+    # zeroes out all negative values in scores : N x C array
+    @staticmethod
+    def forward( scores ) :
+        return np.maximum(0, scores)
+
+    # zeroes out gradients of all scores which are zero
+    @staticmethod
+    def backward( gradients, scores ) :
+        gradients[scores == 0] = 0
         return gradients

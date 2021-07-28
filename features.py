c11c5340e61eb6743bdf7fadbc804f53206a13a0
pradyudasu <pradyu1104@gmail.com>
2021-07-28 22:10:39 +0530

    `HOG` function can now handle sequence of images as input

--- features.py
+++ features.py
@@ -3,22 +3,21 @@ import numpy as np
 
 def __polar_gradient( image ) :
     gradX = np.zeros_like(image)
-    gradX[:, 1 : -1] = image[:, 2 :] - image[:, : -2]
-    gradX[0, :] = gradX[-1, :] = 0
+    gradX[:, :, 1 : -1] = image[:, :, 2 :] - image[:, :, : -2]
+    gradX[:, 0, :] = gradX[:, -1, :] = 0
 
     gradY = np.zeros_like(image)
-    gradY[1 : -1, :] = image[2 :, :] - image[: -2, :]
-    gradY[:, 0] = gradY[:, -1] = 0
+    gradY[:, 1 : -1, :] = image[:, 2 :, :] - image[:, : -2, :]
+    gradY[:, :, 0] = gradY[:, :, -1] = 0
 
     Mag = np.sqrt(gradX * gradX + gradY * gradY)
     Ang = np.arctan2(gradY, gradX) * 180 / np.pi
     Ang[Ang < 0] += 180
     Ang[Ang == 180] = 0
 
-    H, W, C = image.shape
-    max_grad_mask = Mag.argmax(axis=-1)[..., np.newaxis] == np.arange(C)
-    Mag = Mag[max_grad_mask].reshape(H, W)
-    Ang = Ang[max_grad_mask].reshape(H, W)
+    max_grad_mask = Mag.argmax(axis=-1)[..., np.newaxis] == np.arange(image.shape[-1])
+    Mag = Mag[max_grad_mask].reshape(*image.shape[:-1])
+    Ang = Ang[max_grad_mask].reshape(*image.shape[:-1])
     return Mag, Ang
 
 
@@ -33,30 +32,32 @@ def __get_histograms( magnitudes, directions, hist_bins, cell_size ) :
     cell_grid[cell_grid < 0] = 0
     cell_grid *= magnitudes[..., np.newaxis]
 
-    H, W = magnitudes.shape
-    cell_grid = cell_grid.reshape(H // cell_size, cell_size, W // cell_size, cell_size, hist_bins)
-    return np.sum(cell_grid, axis=(1, 3))
+    N, H, W = magnitudes.shape
+    cell_grid.resize(N, H // cell_size, cell_size, W // cell_size, cell_size, hist_bins)
+    return np.sum(cell_grid, axis=(2, 4))
 
 
 def __normalize_histograms( grid, norm_block ) :
-    H, W, bins = grid.shape
+    N, H, W, bins = grid.shape
     idxH = np.arange(H - norm_block + 1)[:, np.newaxis] + np.arange(norm_block)
     idxH = idxH[:, np.newaxis, :, np.newaxis]
     idxW = np.arange(W - norm_block + 1)[:, np.newaxis] + np.arange(norm_block)
     idxW = idxW[np.newaxis, :, np.newaxis, :]
+    idxN = np.arange(N)[:, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
 
-    blocks = grid[idxH, idxW]
-    blocks = blocks.reshape(-1, norm_block * norm_block, bins)
+    blocks = grid[idxN, idxH, idxW]
+    blocks.resize(*blocks.shape[:3], norm_block * norm_block, bins)
     blocks_norm = np.linalg.norm(blocks, axis=(1, 2), keepdims=True)
     np.divide(blocks, blocks_norm, out=blocks, where=(blocks_norm != 0))
-    return blocks.flatten()
+    return blocks.reshape(N, -1)
 
 
 def HOG( image, orientations=9, cell_size=8, *, norm_block=2, color=True ) :
-    if (image.shape[0] % cell_size) + (image.shape[1] % cell_size) != 0 :
-        raise ValueError("Image cannot be divided into exact cells. Check dimensions.\n")
-
     if not color : image = image[..., np.newaxis]
+    if image.ndim == 3 : image = image[np.newaxis, ...]
+
+    if (image.shape[1] % cell_size) + (image.shape[2] % cell_size) != 0 :
+        raise ValueError("Image cannot be divided into exact cells. Check dimensions.\n")
 
     mags, dirs = __polar_gradient(image)
     grid = __get_histograms(mags, dirs, orientations, cell_size)
